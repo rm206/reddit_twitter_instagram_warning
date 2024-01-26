bed7623d3889f47bb5b6ca3cf62080f107767a0a
Rishabh <panicpark60@gmail.com>
2024-01-26 01:57:12 -0600

    done

--- background.js
+++ background.js
@@ -1,5 +1,22 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url && (changeInfo.url.includes("reddit.com") || changeInfo.url.includes("twitter.com"))) {
-        chrome.tabs.sendMessage(tabId, { action: "warn" });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab && tab.url && (tab.url.includes('reddit.com') || tab.url.includes('twitter.com') || tab.url.includes('instagram.com')) && changeInfo.status === 'complete') {
+        chrome.system.display.getInfo(function (displays) {
+            const primaryDisplay = displays.find(display => display.isPrimary);
+            const screenWidth = primaryDisplay.bounds.width;
+            const screenHeight = primaryDisplay.bounds.height;
+            const windowWidth = 1000;
+            const windowHeight = 1000;
+            const left = (screenWidth - windowWidth) / 2;
+            const top = (screenHeight - windowHeight) / 2;
+
+            chrome.windows.create({
+                url: chrome.runtime.getURL("popup.html"),
+                type: "popup",
+                width: windowWidth,
+                height: windowHeight,
+                left: Math.round(left),
+                top: Math.round(top)
+            });
+        });
     }
 });
